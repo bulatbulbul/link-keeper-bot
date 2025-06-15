1c65f5577a4e047e9343edd7b01d3e434df9b93a
Bulat Saifullin <bulatsajfullin00@gmail.com>
2025-06-15 04:04:20 +0300

    add EventProcessor

---
+++ events/telegram/messages.go
@@ -0,0 +1,14 @@
+package telegram
+
+//ToDO
+
+const msgHelp = `Я могу сохранять ваши ссылки 💌 ...`
+
+const msgHello = "Привет! 👋 \n\n" + msgHelp
+
+const (
+	msgUnknownCommand = "Неизвестная команда ❌"
+	msgNoSavedPages   = "Нет ссылок ❗️"
+	msgSaved          = "Сохранил! ✅"
+	msgAlreadyExists  = "Эта ссылка уже сохранена ‼️"
+)
